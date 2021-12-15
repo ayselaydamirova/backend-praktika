f8df9aa3d4cea5b600bec017ad4d074e8e12340d
Aydamirova <tu201903184@aztu.local>
2021-12-15 01:39:38 -0800

    sasasas

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController: Controller
+    {
+        //public ContentResult Index()
+        //{
+        //    var result = new ContentResult();
+        //    result.Content = "hello p201 !!!";
+
+        //    return result;
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    var result = new ViewResult();
+        //    result.ViewName = "Index";
+
+        //    return result;
+        //}
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(int id, string name)
+        {
+            //return Content(name + " - "+id.ToString());
+
+            ViewData["id"] = id;
+            ViewBag.Id = 20;
+            TempData["id"] = 21;
+
+            return RedirectToAction("about");
+        }
+
+        //public ContentResult About()
+        //{
+        //    return Content("About page");
+        //}
+
+        public ViewResult About()
+        {
+
+            return View();
+        }
+
+
+        public JsonResult Json()
+        {
+            return Json(new { name = "Aysel", surname = "Aydamirova" });
+        }
+
+    }
+}
+    
+}
---
+++ WebApplication2/WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController
+    {
+        private List<Student> _students;
+        public StudentController()
+        {
+            _students = new List<Student>
+            {
+                new Student{Id = 1,Name="Hesen",Surname="Seyfullayev",GroupNo="P201"},
+                new Student{Id = 2,Name="Elnur",Surname="Huseynov",GroupNo="P200"},
+                new Student{Id = 3,Name="Elnur",Surname="Akula",GroupNo="P202"},
+                new Student{Id = 4,Name="Togrul",Surname="Niva",GroupNo="P201"}
+            };
+        }
+}
